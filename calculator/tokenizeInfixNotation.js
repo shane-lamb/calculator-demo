b87de5e5ba31be4b4154a4c7d08eaade7f2b8488
Shane <shanelamb1@gmail.com>
2016-01-30 11:00:59 +1000

    added method to tokenize infix notation'

--- calculator/tokenizeInfixNotation.js
+++ calculator/tokenizeInfixNotation.js
@@ -1,3 +1,35 @@
-module.exports = function() {
+var _ = require('lodash');
 
+module.exports = function(infixString) {
+    var tokens = [];
+
+    var tempNumStr = "";
+    var len = infixString.length;
+    for(var i = 0; i < len; i++) {
+        var char = infixString[i];
+
+        var charIsSpace = char.trim() == "";
+        var charIsNum = (!charIsSpace && !isNaN(char)) || char == ".";
+
+        if (charIsNum) {
+            tempNumStr += char;
+        } else {
+            if (tempNumStr) {
+                tokens.push(Number(tempNumStr));
+                tempNumStr = "";
+            }
+
+            if (!charIsSpace) {
+                // char is assumed to be operator
+                tokens.push(char);
+            }
+        }
+    }
+
+    // if loop ends before number is parsed
+    if (tempNumStr) {
+        tokens.push(Number(tempNumStr));
+    }
+
+    return tokens;
 };
