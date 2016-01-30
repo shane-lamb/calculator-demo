ec35dd2ab709b33268bcd173b5e1ba198566179d
Shane <shanelamb1@gmail.com>
2016-01-30 14:24:02 +1000

    finished doReversePolishCalc

--- calculator/doReversePolishCalc.js
+++ calculator/doReversePolishCalc.js
@@ -1,3 +1,18 @@
-module.exports = function() {
+module.exports = function(tokens, operators) {
+    var numStack = [];
 
+    tokens.forEach(function (token) {
+        if (typeof token == "number") {
+            numStack.push(token);
+            return;
+        }
+
+        // token is operator
+        var opCalc = operators[token].calc;
+        var rightVal = numStack.pop();
+        var leftVal = numStack.pop();
+        numStack.push(opCalc(leftVal, rightVal));
+    });
+
+    return numStack[0];
 };
---
+++ calculator/test/doReversePolishCalc.js
@@ -0,0 +1,26 @@
+var should = require('chai').should();
+var expect = require('chai').expect;
+
+var doCalc = require('../doReversePolishCalc');
+
+var ops = {
+    "-": {
+        calc: function(left, right) {
+            return left - right;
+        }
+    },
+    "*": {
+        calc: function(left, right) {
+            return left * right;
+        }
+    }
+};
+
+describe('doReversePolishCalc', function () {
+    it('handles single operation in correct order', function() {
+        doCalc([10, 1, '-'], ops).should.equal(9);
+    });
+    it('handles multiple operations in correct order', function() {
+        doCalc([2, 3, 4, '*', '-'], ops).should.equal(-10);
+    });
+});
